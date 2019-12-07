b80c1846f90436bb0064f604f2f16f37a900c78f
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-07 02:08:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   guess = rand(6) + 1
   puts "Guess a number between 1 and 6:"
   user_input = gets.chomp
-  if user_input == guess
+  if user_input == guess.to_s
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
