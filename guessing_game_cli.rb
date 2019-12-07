27ac6ca35dc067c2b0fcb9b78e3c3fa7bb72160d
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-07 02:07:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   user_input = gets.chomp
   if user_input == guess.to_s
     puts "You guessed the correct number!"
-  elsif user_input.downcase == "exit"
+  elsif user_input == "exit"
     puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{guess}."
